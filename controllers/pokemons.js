4e75b3ba47bb4a461ec3560ac3f2589829cfd4a7
Montaser Amro <mono.amro@gmail.com>
2021-02-09 18:42:47 +0200

    adds pokemons controller

---
+++ controllers/pokemons.js
@@ -0,0 +1,18 @@
+const pokemons = require('../pokedex.json');
+const pokemonsContoller = {
+    getAll: (req, res) => {
+        res.json(pokemons);
+    },
+    getPokemonById: (req, res) => {
+        const { id } = req.params;
+        const pokemon = pokemons.find(poke => poke.id == id)
+        res.json(pokemon)
+    }, 
+    getPokemonInfoByIdAndInfo: (req, res) => {
+        const {id, info} = req.params;
+        const pokemon = pokemons.find(poke => poke.id == id);
+        res.json(pokemon[info])
+    }
+}
+
+module.exports = pokemonsContoller;
