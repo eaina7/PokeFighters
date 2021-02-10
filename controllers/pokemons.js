efdea6a57ff2e3e49de06db7671c2918326fe34a
Montaser Amro <mono.amro@gmail.com>
2021-02-10 15:24:06 +0200

    adds new verification on ID numbers

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -6,8 +6,7 @@ const pokemonsContoller = {
     getPokemonById: (req, res) => {
         try {
             const { id } = req.params;
-            console.log()
-            if (isNaN(id) || id < 1) {throw {code: 400, message:'Please enter a valid id number'}}
+            if (isNaN(id) || id < 1 || id > 809) {throw {code: 400, message:'Please use a correct id number'}}
             const pokemon = pokemons.find(poke => poke.id == id)
             res.json(pokemon)
         } catch (e) {
@@ -17,7 +16,7 @@ const pokemonsContoller = {
     getPokemonInfoByIdAndInfo: (req, res) => {
         try {
             const {id, info} = req.params;
-            if (isNaN(id) || id < 1) {throw {code: 400, message:'Please enter a valid id number'}}
+            if (isNaN(id) || id < 1 || id > 809) {throw {code: 400, message:'Please enter a valid id number'}}
             if (!(["name", "type", "base"].includes(info))) {throw {code: 400, message:'Wrong info type, choose between [name, type, base]'}}
             const pokemon = pokemons.find(poke => poke.id == id);
             res.json(pokemon[info])
