b81eca3ebc3fabeb34f740011e9b378bab430a7b
Jasmin Jenull <jasminjenull@gmail.com>
2021-02-22 08:07:51 +0100

    Add json to body-parser

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const pokemonRoutes = require("./routes/pokemons");
 const resultsRoutes = require("./routes/results");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-app.use(cors())
+app.use(cors());
 
-const mongoDB = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.xm6he.mongodb.net/pokemons?retryWrites=true&w=majority"
+const mongoDB =
+  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.xm6he.mongodb.net/pokemons?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/fight", resultsRoutes); 
-app.use("/", pokemonRoutes); 
+app.use(bodyParser.json());
+app.use("/fight", resultsRoutes);
+app.use("/", pokemonRoutes);
 
 app.listen(port, () => console.log(`Server listening to ${port}!`));
